12c901c5eff5cbd08c283dfeacb8a37e8db49f46
Trygve Vang <trygvevang@10.253.29.224>
2016-02-08 15:19:00 +0100

    Test.

--- GameOfLife/src/gameoflife/GameOfLife.java
+++ GameOfLife/src/gameoflife/GameOfLife.java
@@ -3,6 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+//Kommentar for å sjekke at det funker.
 package gameoflife;
 
 import javafx.application.Application;
