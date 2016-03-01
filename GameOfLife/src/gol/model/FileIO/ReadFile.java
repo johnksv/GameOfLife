c3139eac809c397df464a507de17d519da6af003
John Kasper Svergja <john.k.svergja@gmail.com>
2016-03-01 16:25:17 +0100

    Opprettelse til filbehandling

--- GameOfLife/src/gol/model/FileIO/ReadFile.java
+++ GameOfLife/src/gol/model/FileIO/ReadFile.java
@@ -1,8 +1,20 @@
 package gol.model.FileIO;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Path;
+
 /**
  * @author s305054, s305084, s305089
  */
 public class ReadFile {
+
+    public void readGameBoard(Reader r) throws IOException {
     
+    }
+
+    public void readGameBoardFromDisk(Path file) throws IOException {
+        
+    }
 }
