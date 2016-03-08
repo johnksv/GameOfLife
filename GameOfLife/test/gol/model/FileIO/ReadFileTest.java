0226a673db23a692fa2625123b6b4f76b723843f
John Kasper Svergja <john.k.svergja@gmail.com>
2016-03-08 11:03:56 +0100

    Endret forfatter. Oppsett for test av private metoder

--- GameOfLife/test/gol/model/FileIO/ReadFileTest.java
+++ GameOfLife/test/gol/model/FileIO/ReadFileTest.java
@@ -4,6 +4,7 @@
  */
 package gol.model.FileIO;
 
+import java.lang.reflect.Method;
 import java.net.URI;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -16,56 +17,63 @@ import static org.junit.Assert.*;
 
 /**
  *
- * @author Stian
+ * @author s305054, s305084, s305089
  */
 public class ReadFileTest {
-    
+
     public ReadFileTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
-    public void setUp() {
+    public void setUp() throws Exception {
+        readRLEMethod = ReadFile.class.getDeclaredMethod("readRLE", String[].class);
+        readRLEMethod.setAccessible(true);
+        readPlainText = ReadFile.class.getDeclaredMethod("readPlainText", String[].class);
+        readPlainText.setAccessible(true);
     }
-    
+
     @After
     public void tearDown() {
     }
+
+    Method readRLEMethod;
+    Method readPlainText;
 
     @Test
     public void testReadFileFromDisk() throws Exception {
         System.out.println("readFileFromDisk");
         String url = "file:///glider.cells";
-        Path file= Paths.get(URI.create(url));
-        byte[][] expResult = {{0,64,0},{0,0,64},{64,64,64}};
+        Path file = Paths.get(URI.create(url));
+        byte[][] expResult = {{0, 64, 0}, {0, 0, 64}, {64, 64, 64}};
         byte[][] result = ReadFile.readFileFromDisk(file);
         assertArrayEquals(expResult, result);
     }
-    
-        @Test
-    public void testReadFileFromDisk2() throws Exception {
-        //Forventer exception
-        System.out.println("readFileFromDisk");
-        String url = "file:///Users/Stian/Documents/glider.cell";
-        Path file= Paths.get(URI.create(url));
-        byte[][] expResult = {{0,64,0},{0,0,64},{64,64,64}};
-        byte[][] result = ReadFile.readFileFromDisk(file);
+
+    @Test
+    public void testReadRLE() throws Exception {
+        System.out.println("testReadRLE");
+        String[] a = new String[]{"x = 3, y = 3", "1b1o$2b1o$3o!"};
+        Object[] param = {a};
+
+        byte[][] expected = {{0, 64, 0}, {0, 0, 64}, {64, 64, 64}};
+
+        byte[][] output = (byte[][]) readRLEMethod.invoke(null, new Object[]{a});
+        for (byte[] line : output) {
+            for (byte value : line) {
+                System.out.print(value);
+            }
+            System.out.println("");
+        }
+
+        //  fail("Not implemented yet ");
     }
-        @Test
-    public void testReadFileFromDisk3() throws Exception {
-        System.out.println("readFileFromDisk");
-        String url = "file:///Users/Stian/Documents/glider2.cells";
-        Path file= Paths.get(URI.create(url));
-        byte[][] expResult = {{0,64,0},{0,0,64},{64,64,64}};
-        byte[][] result = ReadFile.readFileFromDisk(file);
-        
-    }
-    
+
 }
