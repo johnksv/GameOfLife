43b55aa7c203173d0a5fe0d40cb8fc19fe1f5039
John Kasper Svergja <john.k.svergja@gmail.com>
2016-03-12 15:00:50 +0100

    Delt opp FXML i nye filer, med egne controllere

---
+++ GameOfLife/src/gol/controller/QuickStatsController.java
@@ -0,0 +1,26 @@
+/*
+ * Here comes the text of your license
+ * Each line should be prefixed with  * 
+ */
+package gol.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author John Kasper
+ */
+public class QuickStatsController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
