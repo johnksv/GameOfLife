8d0dd9ea021454d2ccad1fa4320182bfdd2f0ede
Trygve Vang <trygvevang@10.253.29.224>
2016-03-12 10:21:10 +0100

    Javadoc kommentarer i Rule og ConwaysRule

--- GameOfLife/src/gol/model/Logic/ConwaysRule.java
+++ GameOfLife/src/gol/model/Logic/ConwaysRule.java
@@ -1,14 +1,29 @@
 package gol.model.Logic;
 
 /**
+ * This class implements all the logic behind Conways rules of Game of Life.
+ * 
  * @author s305054, s305084, s305089
  */
 public class ConwaysRule implements Rule {
-
+    
+    /**
+     * Determines whether a living will survive the next generation or not.
+     * 
+     * 
+     * @param cellToCheck cell that is being checked for living neighbors
+     * @return true if living neighbors is two or three, else false
+     */
     private boolean toLive(byte cellToCheck) {
         return cellToCheck == 66 || cellToCheck == 67;
     }
 
+    /**
+     * Check if a dead cell will be spawned the next generation.
+     * 
+     * @param cellToCheck cell that is being checked for living neighbors
+     * @return true if there are exactly three living neighbors
+     */
     private boolean toSpawn(byte cellToCheck) {
         return cellToCheck == 3;
     }
--- GameOfLife/src/gol/model/Logic/Rule.java
+++ GameOfLife/src/gol/model/Logic/Rule.java
@@ -1,10 +1,19 @@
 package gol.model.Logic;
 
 /**
+ * Super to {@link gol.model.Logic.ConwaysRule} and (CustomRule). All classes
+ * that implements this rule will have to override the setLife method.
  * @author s305054, s305084, s305089
  */
 public interface Rule {
 
+    /**
+     * Assigns the value a cell will have the next generation.
+     * 
+     * @param cellToCheck cell that is being edited
+     * @return the correct value of cell that is being checked
+     * @see gol.model.Board.Board#nextGen() 
+     */
     byte setLife(byte cellToCheck);
 
 }
