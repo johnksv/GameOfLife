6f920a7cbd780942a3bbf28f01947c888b01ca1a
Trygve Vang <trygvevang@10.253.29.224>
2016-03-13 16:49:05 +0100

    Quick fixes på Javadoc kommentarer

--- GameOfLife/src/gol/model/Logic/Rule.java
+++ GameOfLife/src/gol/model/Logic/Rule.java
@@ -1,8 +1,7 @@
 package gol.model.Logic;
 
 /**
- * Super to {@link gol.model.Logic.ConwaysRule} and (CustomRule). All classes
- * that implements this rule will have to override the setLife method.
+ * All classes that implements this interface have to override the setLife method.
  * @author s305054, s305084, s305089
  */
 public interface Rule {
@@ -11,7 +10,7 @@ public interface Rule {
      * Assigns the value a cell will have the next generation.
      * 
      * @param cellToCheck cell that is being edited
-     * @return the correct value of cell that is being checked
+     * @return 64 for alive cells, 0 for dead cells
      * @see gol.model.Board.Board#nextGen() 
      */
     byte setLife(byte cellToCheck);
