478132c46ca48821c8d8ea8b8b374f7ac28767d9
John Kasper Svergja <john.k.svergja@gmail.com>
2016-03-17 17:41:06 +0100

    GUIfiks på Editor

--- GameOfLife/src/gol/s305089/controller/editor/EditorController.java
+++ GameOfLife/src/gol/s305089/controller/editor/EditorController.java
@@ -10,10 +10,13 @@ import gol.model.Board.ArrayBoard;
 import gol.model.Board.Board;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 
 /**
@@ -25,14 +28,25 @@ public class EditorController implements Initializable {
 
     @FXML
     private CanvasController canvasController;
+    @FXML
+    private HBox canvasContainer;
     private Board activeBoard;
+    private final List<Canvas> theStrip = new ArrayList<>();
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        System.out.println("EDITOR INIT");
+        initTheStrip();
+
+    }
+
+    private void initTheStrip() {
+        for (int i = 0; i < 20; i++) {
+            theStrip.add(new Canvas(50, 50));
+        }
+        canvasContainer.getChildren().addAll(theStrip);
     }
 
     public void setActiveBoard(Board activeBoard) {
