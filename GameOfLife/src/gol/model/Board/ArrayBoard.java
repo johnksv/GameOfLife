56dd4d9d4b2c6624cb7c296ad9c8f377e99d8698
Stian Stensli <stian.hs@hotmail.com>
2016-03-18 13:18:30 +0100

    Fixed bug in get bounding box

--- GameOfLife/src/gol/model/Board/ArrayBoard.java
+++ GameOfLife/src/gol/model/Board/ArrayBoard.java
@@ -1,6 +1,7 @@
 package gol.model.Board;
 
 import gol.model.Logic.Rule;
+import java.util.Arrays;
 
 /**
  * //TODO Comment about Arrayboard
@@ -20,8 +21,8 @@ public class ArrayBoard extends Board {
      */
     public ArrayBoard() {
         super();
-        WIDTH = 800;
-        HEIGHT = 800;
+        WIDTH = 100;
+        HEIGHT = 100;
         gameBoard = new byte[WIDTH][HEIGHT];
     }
 
@@ -156,6 +157,7 @@ public class ArrayBoard extends Board {
                     result.append("0");
                 }
             }
+            result.append("\n");
         }
         return result.toString();
     }
@@ -165,11 +167,10 @@ public class ArrayBoard extends Board {
     public byte[][] getBoundingBoxBoard() {
 
         int[] boundingBox = getBoundingBox();
-        byte[][] board = new byte[boundingBox[1]][boundingBox[3]];
-
-        for (int y = boundingBox[0]; y <= boundingBox[1]; y++) {
-            for (int x = boundingBox[2]; x <= boundingBox[3]; x++) {
-                if (gameBoard[y][x] == 64) {
+        byte[][] board = new byte[boundingBox[1] - boundingBox[0] + 1][boundingBox[3] - boundingBox[2] + 1];
+        for (int y = 0; y < board.length; y++) {
+            for (int x = 0; x < board[y].length; x++) {
+                if (gameBoard[boundingBox[0] + y][x + boundingBox[2]] == 64) {
                     board[y][x] = 64;
                 } else {
                     board[y][x] = 0;
@@ -234,6 +235,7 @@ public class ArrayBoard extends Board {
 
     @Override
     public String toString() {
+
         StringBuilder result = new StringBuilder();
         for (byte row[] : gameBoard) {
             for (byte cell : row) {
