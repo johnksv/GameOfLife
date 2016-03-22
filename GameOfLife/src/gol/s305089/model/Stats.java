4ef9a54ed0aac8196d10629f3dfe59a41b42c242
John Kasper Svergja <john.k.svergja@gmail.com>
2016-03-22 21:31:54 +0100

    Count living cells

--- GameOfLife/src/gol/s305089/model/Stats.java
+++ GameOfLife/src/gol/s305089/model/Stats.java
@@ -1,5 +1,6 @@
 package gol.s305089.model;
 
+import com.sun.deploy.util.StringUtils;
 import gol.model.Board.ArrayBoard;
 import gol.model.Board.Board;
 
@@ -11,12 +12,20 @@ public class Stats {
     private Board gameboard;
     private byte[][] pattern;
 
-    public int[][] getStatistics(int iterations){
-        
-        
+    public int[][] getStatistics(int iterations) {
+
+        for (int time = 0; time <= iterations; time++) {
+            for (int j = 0; j < 3; j++) {
+                countLiving(time);
+                changeInLiving(time);
+                similarityMeasure();
+            }
+            gameboard.nextGen();
+        }
+
         return null;
     }
-    
+
     /**
      * Consturcts an new Board instance, and inserts this board
      *
@@ -28,6 +37,21 @@ public class Stats {
         gameboard = new ArrayBoard(10, 10);
         this.pattern = Pattern;
         gameboard.insertArray(pattern, 2, 2);
+    }
+
+    private int countLiving(int time) {
+        int countOfLiving = 0;
+        char[] cell = gameboard.toString().toCharArray();
+        for(char value : cell){
+            if(value == '1'){
+                countOfLiving++;
+            }
+        }
+        return countOfLiving;
+    }
+
+    private void changeInLiving(int time) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
