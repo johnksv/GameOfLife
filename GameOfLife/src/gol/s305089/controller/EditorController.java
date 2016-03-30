f779c87dc877db846c7d4308775b85e43420c461
John Kasper Svergja <john.k.svergja@gmail.com>
2016-03-30 18:23:24 +0200

    Working, but the strip is very buggy

--- GameOfLife/src/gol/s305089/controller/EditorController.java
+++ GameOfLife/src/gol/s305089/controller/EditorController.java
@@ -7,15 +7,21 @@ package gol.s305089.controller;
 import gol.controller.CanvasController;
 import gol.model.Board.ArrayBoard;
 import gol.model.Board.Board;
+import gol.s305089.model.GifMaker;
+import java.io.File;
+import java.io.IOException;
 
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.RadioButton;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 
@@ -40,6 +46,7 @@ public class EditorController implements Initializable {
     private Board gameboard;
     private byte[][] pattern;
     private final List<ImageView> theStrip = new ArrayList<>();
+    private GifMaker gifmaker;
 
     /**
      * Initializes the controller class.
@@ -61,8 +68,27 @@ public class EditorController implements Initializable {
 
     @FXML
     private void updateTheStrip() {
+
         for (int iteration = 0; iteration < 20; iteration++) {
-            gameboard.nextGen();
+            try {
+                gameboard.nextGen();
+                
+                File saveLocation = File.createTempFile("golTheStripPreview", ".gif");
+
+                gifmaker = new GifMaker();
+                gifmaker.setSaveLocation(saveLocation.getAbsolutePath());
+                gifmaker.setPattern(gameboard.getBoundingBoxBoard());
+                gifmaker.writePatternToGIF(1);
+                Image previewGif = new Image(saveLocation.toURI().toString());
+                
+                
+                theStrip.get(iteration).setImage(previewGif);
+                
+                saveLocation.delete();
+
+            } catch (IOException ex) {
+                Logger.getLogger(EditorController.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
 
     }
