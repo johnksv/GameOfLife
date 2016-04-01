024be4c179b3b9a708bee289367bc7d2b956f54c
John Kasper Svergja <john.k.svergja@gmail.com>
2016-04-01 08:50:18 +0200

    Added own StripController. not connected yet

---
+++ GameOfLife/src/gol/s305089/controller/TheStripController.java
@@ -0,0 +1,92 @@
+/*
+ * Here comes the text of your license
+ * Each line should be prefixed with  * 
+ */
+package gol.s305089.controller;
+
+import gol.model.Board.ArrayBoard;
+import gol.model.Board.Board;
+import gol.s305089.model.GifMaker;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
+/**
+ * FXML Controller class
+ *
+ * @author John Kasper
+ */
+public class TheStripController implements Initializable {
+
+    @FXML
+    private HBox canvasPreviewContainer;
+
+    private final List<ImageView> theStrip = new ArrayList<>();
+    private GifMaker gifmaker;
+    private Board gameboard;
+    private byte[][] pattern;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    private void initTheStrip() {
+        for (int i = 0; i < 20; i++) {
+            theStrip.add(i, new ImageView());
+        }
+        canvasPreviewContainer.getChildren().addAll(theStrip);
+    }
+
+    @FXML
+    private void updateTheStrip() {
+
+        for (int iteration = 0; iteration < 20; iteration++) {
+            try {
+                gameboard.nextGen();
+
+                File saveLocation = File.createTempFile("golTheStripPreview", ".gif");
+
+                gifmaker = new GifMaker();
+                gifmaker.setSaveLocation(saveLocation.getAbsolutePath());
+                gifmaker.setPattern(gameboard.getBoundingBoxBoard());
+                gifmaker.writePatternToGIF(1);
+                Image previewGif = new Image(saveLocation.toURI().toString());
+
+                theStrip.get(iteration).setImage(previewGif);
+
+                saveLocation.delete();
+
+            } catch (IOException ex) {
+                Logger.getLogger(EditorController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    /**
+     * Constructs an new Board instance, and inserts this board
+     *
+     * @see gol.model.Board.Board#insertArray(byte[][], int, int)
+     * @param Pattern the pattern to set
+     */
+    public void setPattern(byte[][] Pattern) {
+        //TODO dynaimc size of board
+        gameboard = new ArrayBoard(100, 100);
+        this.pattern = Pattern;
+        gameboard.insertArray(pattern, 1, 1);
+    }
+
+}
