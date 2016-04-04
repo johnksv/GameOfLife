acfeb5354a131f0f606a6d339471a56743bc0783
Trygve Vang <trygvevang@10.253.29.224>
2016-04-04 12:02:06 +0200

    Start på GIF - har problemer med Libraryen til Henrik - må prøve å legge til på nytt.

---
+++ GameOfLife/src/gol/s305054/GIFWriterS305054.java
@@ -0,0 +1,19 @@
+package gol.s305054;
+
+import gol.model.Board.ArrayBoard;
+import gol.model.Board.Board;
+
+/**
+ * This class is used to create a .gif file showing several generations of a GoL board.
+ * @author Trygve Vang - s305054
+ */
+public class GIFWriterS305054 {
+    private Board copiedBoard;
+    
+    int width = 100; //Width of .gif - Hardcoded value will be changed
+    int height = 100; //Height of .gif - Hardcoded value will be changed
+    int time = 1000; // 1000 ms = 1s
+    
+    //lieng.GIFWriter gifWriter;
+    
+}
