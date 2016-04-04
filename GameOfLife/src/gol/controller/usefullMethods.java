ddc35eebffd7cbbf2ad6e2cbfbd86a0ec80145af
John Kasper Svergja <john.k.svergja@gmail.com>
2016-04-04 22:11:32 +0200

    Rotasjon av array fungerende

--- GameOfLife/src/gol/controller/usefullMethods.java
+++ GameOfLife/src/gol/controller/usefullMethods.java
@@ -5,16 +5,28 @@ package gol.controller;
  */
 public final class usefullMethods {
 
-    private usefullMethods(){}
- 
-    private static <T> byte[][] rotateArray90Deg(T[][] arrayToRotate){
-        byte[][] rotatedArray = new byte[arrayToRotate.length][];
-        for (int i = 0; i < arrayToRotate.length; i++) {
-            
+    private usefullMethods() {
+    }
+
+    public static byte[][] rotateArray90Deg(byte[][] arrayToRotate) {
+        int longestRow = 0;
+        for (byte[] row : arrayToRotate) {
+            if (row.length > longestRow) {
+                longestRow = row.length;
+            }
         }
-        
+
+        byte[][] rotatedArray = new byte[longestRow][arrayToRotate.length];
+
+        for (int i = 0; i < arrayToRotate.length; i++) {
+            byte[] row = new byte[arrayToRotate[i].length];
+            for (int j = 0; j < arrayToRotate[i].length; j++) {
+                row[j] = arrayToRotate[i][j];
+                rotatedArray[j][arrayToRotate.length - 1 - i] = row[j];
+            }
+        }
+
         return rotatedArray;
     }
-    
-    
+
 }
