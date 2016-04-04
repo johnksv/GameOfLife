7b2a78ad4a6d6edce3fa76c836c0f8db0f3e9022
John Kasper Svergja <john.k.svergja@gmail.com>
2016-04-04 21:19:32 +0200

    Class for usefull methods. Start of method rotateArray90Deg

---
+++ GameOfLife/src/gol/controller/usefullMethods.java
@@ -0,0 +1,20 @@
+package gol.controller;
+
+/**
+ * @author s305089
+ */
+public final class usefullMethods {
+
+    private usefullMethods(){}
+ 
+    private static <T> byte[][] rotateArray90Deg(T[][] arrayToRotate){
+        byte[][] rotatedArray = new byte[arrayToRotate.length][];
+        for (int i = 0; i < arrayToRotate.length; i++) {
+            
+        }
+        
+        return rotatedArray;
+    }
+    
+    
+}
