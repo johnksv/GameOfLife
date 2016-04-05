82b4756a292a3b1f9f9d63bf7caab5e226ab8d69
Trygve Vang <trygvevang@10.253.29.224>
2016-04-05 23:55:02 +0200

    Pseudo code just to make it easier when starting to code properly.

--- GameOfLife/src/gol/s305054/GIFWriterS305054.java
+++ GameOfLife/src/gol/s305054/GIFWriterS305054.java
@@ -9,12 +9,34 @@ import lieng.GIFWriter;
  * @author Trygve Vang - s305054
  */
 public class GIFWriterS305054 {
-    private Board copiedBoard;
+    private Board copiedBoard; //deep copy of board - TODO check if boundBox or not.
     
     int width = 100; //Width of .gif - Hardcoded value will be changed
     int height = 100; //Height of .gif - Hardcoded value will be changed
-    int time = 1000; // 1000 ms = 1s
+    int time = 1000; // 1000 ms = 1s - later, listener to a slider
+    String path = "testGif.gif"; //Filepath - later, normal output stream
     
-    GIFWriter gifWriter;
+    private GIFWriter gifWriter;
+    
+    public void setCopiedBoard(Board originaleBoard) {
+        /*
+        Param originaleBoard - get array, each element in originaleArray, assigned to copied array
+        assigns copied array to copiedBoard.
+        */
+    }
+    public void makeGif() { //method should be changed to propely work recursion wise
+        if() { //number of pictures left == 1
+            gifWriter.insertAndProceed();
+            gifWriter.close();
+            //return the finished gif, ready to be exported            
+        } else {
+            gifWriter.createNextImage(); //creates the image
+            gifWriter.insertAndProceed(); //insert the image to gifStream?
+            
+            copiedBoard.nextGen(); //calculates the next generation of the board.
+            //number of pictures left -=1;
+            makeGif(); //recursive call.
+        }
+    }
     
 }
