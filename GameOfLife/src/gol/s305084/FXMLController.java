cca5fb9708c73024d3919c5de11770813f5584fb
Stian Stensli <stian.hs@hotmail.com>
2016-04-06 12:35:43 +0100

    Editor fxml ferdig

---
+++ GameOfLife/src/gol/s305084/FXMLController.java
@@ -0,0 +1,22 @@
+package gol.s305084;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author S305084
+ */
+public class FXMLController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
