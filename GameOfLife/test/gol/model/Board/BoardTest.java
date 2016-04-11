4c23da8ef318c86ad92d004c717c1a58d0efc478
Trygve Vang <trygvevang@10.253.29.224>
2016-04-11 11:34:58 +0200

    ASDF

--- GameOfLife/test/gol/model/Board/BoardTest.java
+++ GameOfLife/test/gol/model/Board/BoardTest.java
@@ -111,11 +111,9 @@ public class BoardTest {
     public void testGetArrayLength_0args() {
         System.out.println("getArrayLength");
 
-        int expResult = 0;
+        int expResult = 5;
         int result = arrayInstance.getArrayLength();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
