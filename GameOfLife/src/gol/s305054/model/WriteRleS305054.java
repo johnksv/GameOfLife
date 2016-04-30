dc6b744d56e2c30712cfd8d26dd0a4d77ef26784
Trygve Vang <trygvevang@10.253.29.224>
2016-04-30 21:22:16 +0200

    Mesteparten av meta data er handla i write rle

--- GameOfLife/src/gol/s305054/model/WriteRleS305054.java
+++ GameOfLife/src/gol/s305054/model/WriteRleS305054.java
@@ -3,6 +3,7 @@ package gol.s305054.model;
 
 import gol.model.Board.Board;
 import java.io.IOException;
+import javafx.scene.control.TextField;
 
 /**
  * @author Trygve Vang - s305054
@@ -15,8 +16,11 @@ public class WriteRleS305054 {
     /**
      * Parses this board to a RLE file.
      * @param boardToParse a playable board
+     * @param title title of this pattern
+     * @param author author of this pattern
+     * @param description description of this pattern
      */
-    public void writeRLE(Board boardToParse) throws IOException {
+    public void writeRLE(Board boardToParse, TextField title, TextField author, TextField description) throws IOException {
         //TODO title, author, etc
         //TODO boundingboxBoard, parse rle from that
         boundingBox = boardToParse.getBoundingBoxBoard();
@@ -24,6 +28,17 @@ public class WriteRleS305054 {
         //y and x (metadata)
         int y = boundingBox.length;
         int x = boardToParse.getArrayLength(0); 
+        
         StringBuilder pattern = new StringBuilder();
+        pattern.append("#N " + title.getText() + "\n#O " + author.getText() + "\n#C " + description.getText() + "\nx = " + x + ", y = " + y);
+        /*
+         * A finished .rle file is looking like this:
+        
+            #N Name
+            #O Author
+            #C Comment
+            x = How many columns, y = how many rows, rule = B(what number its born on)/S(what number it survives on)
+            (actual pattern where b is dead, o is alive, $ is new line, and ! ends the pattern)
+         */
     }
 }
